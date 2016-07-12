a30c5a3b182ee1c8c090be23efd0437f4cf6d3fe
Simone Accascina <simon@accascina.me>
2016-07-12 15:32:37 +0200

    Implement functions in MonsterModel

--- js/app.js
+++ js/app.js
@@ -1,29 +1,44 @@
 /*
  * MonsterModel provides basic methods to iterate
- * over a collection of "monsters"  
+ * over a collection of "monsters"
  */
 var MonsterModel = {
-  
+
    /* Collection of available monsters */
-   collection : monsters, 
-  
+   collection : monsters,
+   currentMonsterIndex: null,
+
    /* Start the iteration over the collection of monsters.
-   it sets the first element as the current monster */    
+   it sets the first element as the current monster */
    init : function() {
+     this.currentMonsterIndex = 0;
    },
-  
+
    /*   Returns the current monster and sets as current monster
     * the next element in the array.
     * If no more monsters are available, it returns null
     */
-   getCurrentMonster : function () { 
+   getCurrentMonster : function () {
+     return this.collection[this.currentMonsterIndex];
    },
-  
-   /* Checks if the name of the current monster 
-    * is monsterName. It returns true if the user 
+
+   /* Checks if the name of the current monster
+    * is monsterName. It returns true if the user
     * correctly named the monster, false otherwise
     */
    catchMonster : function (monsterName){
-   }  
-           
+    if (monsterName == this.getCurrentMonster().name){
+       alert('Caught!');
+
+       if (this.currentMonsterIndex >= this.collection.length - 1){
+         alert('No more monsters');
+       }
+       else {
+         this.currentMonsterIndex += 1;
+       }
+     }
+     else {
+       alert('Sorry, master');
+     }
+   },
 };
